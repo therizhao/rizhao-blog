4c6ffb2f3cd69e018c7a915690912e2941d3179a
rizhaow <rizhaow@gmail.com>
2019-03-21 22:27:25 +0800

    intiialise git

---
+++ src/siteMetadata.js
@@ -0,0 +1,11 @@
+export default {
+  // The blog's title as it appears in the layout header, and in the document
+  // <title> tag.
+  title: "rizhao's blog",
+
+  author: "rizhao",
+  description: "where rizhao blogs about stuff",
+
+  // The number of posts to a page on the site index.
+  indexPageSize: 10,
+}
